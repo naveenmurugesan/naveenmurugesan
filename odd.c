ce78ddf6da4e66df4520cd366be9de9b8bcbd615
naveenmurugesan <naveenmurugesan4@gmail.com>
2017-06-28 15:39:55 +0530

    odd.c

---
+++ odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a ;
+clrscr();
+printf("\n odd or even numbers");
+printf("\n enter the number you want to check");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n a is even");
+}
+else
+{
+printf("\n a is odd");
+}
+getch();
+}
