de85fafed04c66e3072cfc51b63fabdd7d61b968
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-06-04 15:23:36 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,3 +1,7 @@
-m=input("enter the string to be reverse")
-n=m[::-1]
-print("the rev string is "+",[n]}
+def reverse_slicing(s):
+    return s[::-1]
+
+x = (input("enter the string"))
+
+if __name__ == "__main__":
+    print('Reverse String using slicing =', reverse_slicing(x))
